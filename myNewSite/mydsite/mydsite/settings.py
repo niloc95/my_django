77e69c647b86d49e2f20311cb4dc15e9eac1ec3e
Nilo Cara <nilo.cara@gmail.com>
2023-04-10 10:05:13 +0200

    requirements.txt new file

--- myNewSite/mydsite/mydsite/settings.py
+++ myNewSite/mydsite/mydsite/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*x$vr)$wm%&zn=^&wb@!dele(!_43-5=k2d^@*z!rhxhk5@ux#'
-
+#SECRET_KEY = 'django-insecure-*x$vr)$wm%&zn=^&wb@!dele(!_43-5=k2d^@*z!rhxhk5@ux#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 DEBUG = True
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
