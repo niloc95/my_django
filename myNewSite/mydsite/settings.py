32b55146e096c773532b6937bccdbd7752043623
Nilo <nilo.cara@gmail.com>
2023-04-21 10:22:57 +0200

    Development Branch

--- myNewSite/mydsite/settings.py
+++ myNewSite/mydsite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = bool(int(os.environ.get('DEBUG', 0)))
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['about.frontend.co.za', 'django.frontend.co.za']
+ALLOWED_HOSTS = []
 # ALLOWED_HOSTS.extend(
 #     filter(
 #         None,
